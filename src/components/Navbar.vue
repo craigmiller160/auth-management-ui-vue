5711794f4ef080333e8d15a01e81ecb217282681
Craig Miller <craigmiller160@gmail.com>
2020-06-26 12:54:04 -0400

    More styling tweaks

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,13 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#">Logo</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="#" class="nav-link">Login</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -19,9 +26,14 @@ export default {
 
   .navbar {
     background-color: $orange;
+
     &.navbar-expand {
       .navbar-toggler {
         margin-right: 1rem;
+      }
+
+      .nav-link {
+        color: $white;
       }
     }
   }
