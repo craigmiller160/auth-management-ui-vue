2675198d857130163744ba0cc418efc42964f296
Craig Miller <craigmiller160@gmail.com>
2020-06-26 15:31:49 -0400

    Built all routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/Home';
+import Users from '@/components/Users';
+import Clients from '@/components/Clients';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: Users
+  },
+  {
+    path: '/clients',
+    name: 'Clients',
+    component: Clients
   }
 ];
 
