5591d0549d51a834c705c671b5cb9fe37af8a0aa
Craig Miller <craigmiller160@gmail.com>
2020-06-26 15:29:12 -0400

    Added Vue Router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/scss/bootstrap.scss';
-// import router from './archive/router';
+import router from './router';
 // import store from './archive/store';
 
 // createApp(App).use(router).use(store).mount('#app');
 createApp(App)
+  .use(router)
   .mount('#app');
