4509447c6451ce78b264d3f2941f618e7f71d320
Craig <craigmiller160@gmail.com>
2020-06-26 10:56:36 -0400

    Practicing the composition API

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'no-plusplus': 0
   },
   overrides: [
     {
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <h1>Auth Management UI</h1>
+  <p><strong>Count:</strong> {{ state.count }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
 </template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0
+    });
+
+    const increment = () => state.count++;
+    const decrement = () => state.count--;
+
+    return {
+      state,
+      increment,
+      decrement
+    };
+  }
+};
+</script>
 
 <style lang="scss">
   h1 {
