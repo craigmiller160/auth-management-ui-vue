3835e619108b8cfe9221b40c3ce029a82bd0ca86
Craig Miller <craigmiller160@gmail.com>
2020-06-26 12:25:39 -0400

    Removed all materialize stuff

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,55 +1,14 @@
 <template>
-  <nav>
-    <div class="nav-wrapper orange">
-      <a href="#" class="sidenav-trigger" @click="toggleDrawer">
-        <i class="material-icons">menu</i>
-      </a>
-      <a href="#" class="brand-logo">OAuth2 Management {{ state.drawerOpen }}</a>
-      <ul class="show-drawer right hide-on-med-and-down">
-        <li><a href="#">Login</a></li>
-      </ul>
-    </div>
-  </nav>
-  <ul class="sidenav orange closed">
-    <li><a href="#" class="white-text">Login</a></li>
-  </ul>
+
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue';
 
 export default {
-  name: 'Navbar',
-  setup() {
-    const state = reactive({
-      drawerOpen: false
-    });
-
-    const toggleDrawer = () => {
-      console.log('Click'); // TODO delete this
-      state.drawerOpen = !state.drawerOpen;
-    };
-
-    onMounted(() => {
-
-    });
-
-    return {
-      state,
-      toggleDrawer
-    };
-  }
+  name: 'Navbar'
 };
 </script>
 
 <style scoped lang="scss">
-.nav-wrapper {
-  padding: 0 1rem;
 
-  .side-nav {
-    &.closed {
-      width: 20rem;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min';
 // import router from './archive/router';
 // import store from './archive/store';
 
