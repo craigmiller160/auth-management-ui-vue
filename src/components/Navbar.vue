e42ddd19e5a06e98398f56b8109cafe7ed8282eb
Craig Miller <craigmiller160@gmail.com>
2020-06-26 12:45:17 -0400

    Got bootstrap variables to work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class="navbar navbar-expand navbar-light bg-light">
     <a class="navbar-brand" href="#">Logo</a>
   </nav>
+  <h1>Hello World</h1>
 </template>
 
 <script>
@@ -12,5 +13,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+  @import "../assets/scss/variables";
+  h1 {
+    color: $blue;
+  }
 </style>
