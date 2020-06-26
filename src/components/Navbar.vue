acfbfc48f9b10a7361d19f707bfc4e21174112d6
Craig Miller <craigmiller160@gmail.com>
2020-06-26 15:00:50 -0400

    Improved auth UI

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,18 @@
 <template>
   <nav class="navbar navbar-expand navbar-dark">
+    <a class="navbar-brand" href="#">OAuth Management</a>
     <button class="navbar-toggler" type="button">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Logo</a>
     <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link">Users</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Clients</a>
+        </li>
+      </ul>
       <ul class="navbar-nav">
         <li class="nav-item">
           <a href="#" class="nav-link">Login</a>
