12865e894863a05d3400068ab36f895fd806acff
Craig <craigmiller160@gmail.com>
2020-06-26 10:46:19 -0400

    Moved everything into archive folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+// import router from './archive/router';
+// import store from './archive/store';
 
-createApp(App).use(router).use(store).mount('#app');
+// createApp(App).use(router).use(store).mount('#app');
+createApp(App)
+  .mount('#app');
