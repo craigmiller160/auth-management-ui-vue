70c4221d609fd0db3af892ed29046d5fde04f3db
Craig Miller <craigmiller160@gmail.com>
2020-06-26 12:31:06 -0400

    Began working on new appbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="mui-appbar">
+    <table width="100%">
+      <tr>
+        <td class="mui--appbar-height">Left Side</td>
+        <td class="mui-appbar-height" align="right">Right Side</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
