e0af3dfb61a7e62cba55831455ec2f4fd5f5eaf7
Craig Miller <craigmiller160@gmail.com>
2020-06-26 15:30:58 -0400

    Cleaned up App component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <Navbar />
-  <Home />
-  <Clients />
-  <Users />
+  <router-view></router-view>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar';
-import Home from '@/components/Home';
-import Clients from '@/components/Clients';
-import Users from '@/components/Users';
 
 export default {
-  components: {
-    Users, Clients, Home, Navbar
-  }
+  name: 'App',
+  components: { Navbar }
 };
 </script>
 
