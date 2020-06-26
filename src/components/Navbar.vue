a5b69067a439434b3891ca513d8a50efbc0a11a0
Craig <craigmiller160@gmail.com>
2020-06-26 12:04:13 -0400

    Committing progress

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,51 @@
 <template>
   <nav>
     <div class="nav-wrapper orange">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul class="right hide-on-med-and-down">
+      <a href="#" class="sidenav-trigger" @click="toggleDrawer">
+        <i class="material-icons">menu</i>
+      </a>
+      <a href="#" class="brand-logo">OAuth2 Management {{ state.drawerOpen }}</a>
+      <ul class="show-drawer right hide-on-med-and-down">
         <li><a href="#">Login</a></li>
       </ul>
     </div>
   </nav>
+  <ul class="side-nav orange closed">
+    <li><a href="#" class="white-text">Login</a></li>
+  </ul>
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  setup() {
+    const state = reactive({
+      drawerOpen: false
+    });
+
+    const toggleDrawer = () => {
+      console.log('Click'); // TODO delete this
+      state.drawerOpen = !state.drawerOpen;
+    };
+
+    return {
+      state,
+      toggleDrawer
+    };
+  }
 };
 </script>
 
 <style scoped lang="scss">
 .nav-wrapper {
   padding: 0 1rem;
+
+  .side-nav {
+    &.closed {
+      width: 20rem;
+    }
+  }
 }
 </style>
