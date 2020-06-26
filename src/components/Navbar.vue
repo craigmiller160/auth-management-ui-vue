7423cd2b4e7594e05b5d149b9a571f6fbb3b6229
Craig <craigmiller160@gmail.com>
2020-06-26 11:45:30 -0400

    Began adding Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <div class="nav-wrapper orange">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
+
+<style scoped lang="scss">
+.nav-wrapper {
+  padding: 0 1rem;
+}
+</style>
