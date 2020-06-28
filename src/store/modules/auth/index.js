dbde0a3db7347aa8cd4156ff74aaee2bc4e5ad0b
Craig Miller <craigmiller160@gmail.com>
2020-06-28 13:39:20 -0400

    Began building store

---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+
+// TODO delete this
+const sampleData = { // eslint-disable-line no-unused-vars
+  firstName: 'Craig',
+  lastName: 'Miller',
+  username: 'craig@gmail.com'
+};
+
+const state = {
+  userData: null
+};
+
+const mutations = {
+  setUserData(storeState, userData) {
+    storeState.userData = userData;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations
+};
