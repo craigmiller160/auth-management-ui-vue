bc0164a3359849f673664ab29751ac492481d317
Craig Miller <craigmiller160@gmail.com>
2020-06-28 13:43:00 -0400

    Added store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex';
+import auth from './modules/auth';
 
 const store = createStore({
-  strict: process.env.NODE_ENV === 'development'
+  strict: process.env.NODE_ENV === 'development',
+  modules: {
+    auth
+  }
 });
 
 export default store;
