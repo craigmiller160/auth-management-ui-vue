bce67c79f80759c427e600487bab6f1a5c9c49ac
Craig Miller <craigmiller160@gmail.com>
2020-07-06 12:22:10 -0400

    Added service for getting the authenticated user

--- src/service/BasicService.js
+++ src/service/BasicService.js
@@ -1,7 +1,5 @@
 import api from './api';
 
-// TODO all uses of these services need error handling
-
 export const getUsers = async () => {
   const res = await api.get('/basic/users');
   return res.data;
@@ -9,5 +7,10 @@ export const getUsers = async () => {
 
 export const getClients = async () => {
   const res = await api.get('/basic/clients');
+  return res.data;
+};
+
+export const getAuthUser = async () => {
+  const res = await api.get('/basic/auth');
   return res.data;
 };
