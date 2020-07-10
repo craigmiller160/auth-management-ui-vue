1965041195f11b671ecadd913c40f07b36e92c46
Craig Miller <craigmiller160@gmail.com>
2020-07-10 19:01:55 -0400

    Stubbed out ClientDetails

---
+++ src/components/clients/ClientDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header text="Client Details" />
+  <div class="row">
+    <div class="col">
+      <h3>Content Will Go Here</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/ui/Header';
+
+  export default {
+    name: 'ClientDetails',
+    components: { Header }
+  };
+</script>
+
+<style scoped>
+
+</style>
