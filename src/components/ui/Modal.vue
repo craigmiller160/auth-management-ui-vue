ca228dfc6bfcd1ba4a611f519cd9558108d7b106
Craig Miller <craigmiller160@gmail.com>
2020-07-12 17:17:07 -0400

    Built basic modal

--- src/components/ui/Modal.vue
+++ src/components/ui/Modal.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <div class="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">The Modal</h5>
+          <button type="button" class="close">
+            <span>&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal Body</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Button 1</button>
+          <button type="button" class="btn btn-info">Button 2</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
