fdfd3745a12ba85a4ba4a333d18d8826d108fcdd
Craig Miller <craigmiller160@gmail.com>
2020-07-14 17:41:48 -0400

    Got the basic UserDetails layout done

--- src/components/users/UserDetails.vue
+++ src/components/users/UserDetails.vue
@@ -1,10 +1,87 @@
 <template>
-  <h1>User Details</h1>
+  <Header text="User Details" />
+  <div class="row">
+    <div class="col-4 offset-4">
+      <TextField
+        name="email"
+        label="Email"
+        type="email"
+        v-model="state.user.email"
+        :required="true"
+      />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4 offset-2">
+      <TextField
+        name="password"
+        label="New Password"
+        type="password"
+        v-model="state.user.password"
+      />
+    </div>
+    <div class="col-4">
+      <!-- TODO need to figure out validation for matching the passwords -->
+      <TextField
+        name="confirmPassword"
+        label="Confirm New Password"
+        type="password"
+        v-model="state.confirmPassword"
+      />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4 offset-2">
+      <TextField
+        name="firstName"
+        label="First Name"
+        v-model="state.user.firstName"
+      />
+    </div>
+    <div class="col-4">
+      <TextField
+        name="lastName"
+        label="Last Name"
+        v-model="state.user.lastName"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+  import Header from '@/components/ui/Header';
+  import TextField from '@/components/ui/form/TextField';
+  import { onMounted, reactive } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { getUser } from '@/service/UserService';
+
+  const ID_NEW = 'new';
+
   export default {
-    name: 'UserDetails'
+    name: 'UserDetails',
+    components: { Header, TextField },
+    setup() {
+      const router = useRouter();
+      const state = reactive({
+        user: {},
+        oldUser: {},
+        confirmPassword: ''
+      });
+
+      onMounted(async () => {
+        const { id } = router.currentRoute.value.params;
+        if (id !== ID_NEW) {
+          const user = (await getUser(id)) ?? {};
+          state.user = { ...user };
+          state.oldUser = user;
+        }
+        // TODO do I need special logic for new users?
+      });
+
+      return {
+        state
+      };
+    }
   };
 </script>
 
