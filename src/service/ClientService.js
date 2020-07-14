0c37a28ebbb307422cf6a9f0b77ccad8128b1626
Craig Miller <craigmiller160@gmail.com>
2020-07-14 16:49:29 -0400

    Refactored ClientService to use new API

--- src/service/ClientService.js
+++ src/service/ClientService.js
@@ -1,75 +1,33 @@
 import api from '@/service/api';
-import store from '@/store';
-import { MUTATION_SHOW_ERROR_ALERT } from '@/store/modules/alert/keys';
-
-// export const getClients = async () => {
-//   try {
-//     const res = await api.get('/clients');
-//     return res.data;
-//   } catch (ex) {
-//     console.log(ex);
-//     store.commit(MUTATION_SHOW_ERROR_ALERT, `Error loading clients: ${ex.message}`);
-//     return undefined;
-//   }
-// };
 
 export const getClients = () => api.get({
   uri: '/clients',
   errorMsg: 'Error loading clients'
 });
 
-export const getClient = async (id) => {
-  try {
-    const res = await api.get(`/clients/${id}`);
-    return res.data;
-  } catch (ex) {
-    // TODO merge this into generic error handling that extracts the response from the server
-    console.log(ex);
-    store.commit(MUTATION_SHOW_ERROR_ALERT, `Error loading details for client ${id}: ${ex.message}`);
-    return undefined;
-  }
-};
+export const getClient = (id) => api.get({
+  uri: `/clients/${id}`,
+  errorMsg: 'Error loading details for client'
+});
 
-export const generateGuid = async () => {
-  try {
-    const res = await api.get('/clients/guid');
-    return res.data;
-  } catch (ex) {
-    console.log(ex);
-    store.commit(MUTATION_SHOW_ERROR_ALERT, `Error generating GUID: ${ex.message}`);
-    return undefined;
-  }
-};
+export const generateGuid = () => api.get({
+  uri: '/clients/guid',
+  errorMsg: 'Error generating GUID'
+});
 
-export const updateClient = async (id, client) => {
-  try {
-    const res = await api.put(`/clients/${id}`, client);
-    return res.data;
-  } catch (ex) {
-    console.log(ex);
-    store.commit(MUTATION_SHOW_ERROR_ALERT, `Error updating client ${id}: ${ex.message}`);
-    return undefined;
-  }
-};
+export const updateClient = (id, client) => api.put({
+  uri: `/clients/${id}`,
+  body: client,
+  errorMsg: `Error updating client ${id}`
+});
 
-export const createClient = async (client) => {
-  try {
-    const res = await api.post('/clients', client);
-    return res.data;
-  } catch (ex) {
-    console.log(ex);
-    store.commit(MUTATION_SHOW_ERROR_ALERT, `Error creating client: ${ex.message}`);
-    return undefined;
-  }
-};
+export const createClient = (client) => api.post({
+  uri: '/clients',
+  body: client,
+  errorMsg: 'Error creating client'
+});
 
-export const deleteClient = async (id) => {
-  try {
-    const res = await api.delete(`/clients/${id}`);
-    return res.data;
-  } catch (ex) {
-    console.log(ex);
-    store.commit(MUTATION_SHOW_ERROR_ALERT, `Error deleting client ${id}: ${ex.message}`);
-    return undefined;
-  }
-};
+export const deleteClient = (id) => api.delete({
+  uri: `/clients/${id}`,
+  errorMsg: 'Error deleting client'
+});
